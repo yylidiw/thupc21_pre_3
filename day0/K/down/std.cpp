e401b4532ecdff18506aa495d85b7b8fe83c12bc
shihanyuan <shihanyuan@megvii.com>
2020-11-26 22:29:57 +0800

    add data && std && check for problem K

---
+++ day0/K/down/std.cpp
@@ -0,0 +1,101 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+struct node {
+  node *next;
+  int where, v;
+} *first[100001], a[100001];
+
+int n, m, l, v[100001], c[100001], dist[100001], w[100001], cnt, father[100001], idx[100001], sx[100001];
+bool b[100001];
+long long f[100001][2];
+set < pair<int, int> > events;
+
+inline void makelist(int x, int y, int z) {
+  a[++l].where = y; a[l].v = z; a[l].next = first[x]; first[x] = &a[l];
+}
+
+int uu = 0;
+
+long long calc(int init) {
+  dist[init] = 1; c[1] = init; cnt = 0;
+  int k = 1;
+  for (int l = 1; l <= k; l++) {
+    int m = c[l];
+    for (node *x = first[m]; x; x = x->next)
+      if (!dist[x->where])
+        dist[x->where] = dist[m] + 1, c[++k] = x->where, father[x->where] = m, sx[x->where] = k;
+      else
+        if (father[m] != x->where && sx[x->where] >= sx[m])
+          w[++cnt] = x->where;
+  }
+  sort(w + 1, w + cnt + 1);
+  uu += cnt;
+  int tot = 0;
+  for (int i = 1; i <= cnt; i++)
+    if (i == 1 || w[i] != w[tot])
+      w[++tot] = w[i];
+  for (int i = 1; i <= tot; i++)
+    idx[w[i]] = i;
+  long long res = 0;
+  for (int i = 0; i < 1 << tot; i++) {
+    for (int j = k; j; --j) {
+      int m = c[j];
+      f[m][0] = 0; f[m][1] = v[m];
+      if (idx[m])
+        if (i & (1 << (idx[m] - 1)))
+          f[m][0] = -1LL << 60LL;
+        else
+          f[m][1] = -1LL << 60LL;
+      for (node *x = first[m]; x; x = x->next)
+        if (sx[x->where] >= sx[m]) {
+          if (father[x->where] == m) {
+            f[m][0] += max(f[x->where][0], f[x->where][1]);
+            f[m][1] += max(f[x->where][0], f[x->where][1] - x->v);
+          } else 
+            if (idx[x->where] && (i & (1 << (idx[x->where] - 1))))
+              f[m][1] -= x->v;
+        }
+      //printf("%d %lld %lld\n", m, f[m][0], f[m][1]);
+    }
+    res = max(res, max(f[init][0], f[init][1]));
+  }
+  return res;
+}
+
+long long calcs(long long x) {
+  int P = 1000000007, xs = 101, ex = 137;
+  return (xs * x + ex) % P;
+}
+
+int main() {
+  int x, y, z, r;
+  scanf("%d%d%d%d%d%d", &n, &m, &x, &y, &r, &z);
+  for (int i = 1; i <= n; i++) {
+    r = calcs(r);
+    v[i] = r;
+  }
+  for (int i = 1; i <= m; i++) {
+    x = calcs(x);
+    y = calcs(y);
+    z = calcs(z);
+    int cx = x % n + 1, cy = y % n + 1;
+    if (cx != cy && events.find(make_pair(cx, cy)) == events.end())
+        makelist(cx, cy, z), makelist(cy, cx, z), 
+        events.insert(make_pair(cx, cy)),
+        events.insert(make_pair(cy, cx));
+  }
+  long long ans = 0;
+  for (int i = 1; i <= n; i++)
+    if (!dist[i])
+      ans += calc(i);
+  printf("%lld\n", ans);
+}
--- day0/K/statement/zh-cn.md
+++ day0/K/statement/zh-cn.md
@@ -15,13 +15,13 @@
 
 {{ self.input_file() }}
 
-第一行包含两个整数 $n, m$ $(1\le n\le 100000,1\le m\le \frac{n}{2})$。
+令 $q = 101, b = 137, p = 1000000007$。
 
-接下来一行包含 $n$ 个整数 $a_1,a_2,\ldots,a_n$ $(1\le a_i\le 1000000000)$。
+第一行包含六个整数 $n, m, x_0, y_0, a_0, z_0$ $(1\le n\le 100000,0\le m\le \frac{n}{2}, 0 \le x_0, y_0, r_0, z_0 < P)$。
 
-接下来 $m$ 行，每行三个整数 $ u,v,k~(1\leq u,v \leq n, u \neq v,1\le k\le 1000000000)$，描述了一条连接 $u,v$ 的值为 $k$ 的边。
+对于 $1 \le i \le n$, 有 $a_i = (q\times a_{i-1} + b)\mod p$。
 
-数据保证所有的边都是随机生成的。
+对于 $1 \le i \le m$, 有 $x_i = (q\times x_{i-1} + b)\mod p,y_i = (q\times y_{i-1} + b)\mod p, z_i = (q\times z_{i-1} + b)\mod p$。对于每一组 $(x_i, y_i, z_i)$ 描述了一条连接 $(x_i\mod n)+1,(y_i\mod n)+1$ 的值为 $z_i$ 的边，如果 $x_i=y_i$ 或者之前出现过连接$x_i$和$y_i$的边，则忽视这条边（即这条边不存在）。
 
 {{ s('output format') }}
 
